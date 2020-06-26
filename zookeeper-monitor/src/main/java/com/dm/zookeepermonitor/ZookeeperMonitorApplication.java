73afe132ee9d96e906fcd78b58bb5c470be72a4d
dm13579 <7028858dm>
2020-06-26 12:15:30 +0800

    zookeeper监控系统运行-前端

---
+++ zookeeper-monitor/src/main/java/com/dm/zookeepermonitor/ZookeeperMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.dm.zookeepermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperMonitorApplication.class, args);
+    }
+
+}
